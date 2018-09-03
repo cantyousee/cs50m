f42815cd158510e4bce1f3c4a35365900485d759
himanscience <himanshugholap96@gmail.com>
2018-09-03 21:36:39 +0530

    implemented script

--- script.js
+++ script.js
@@ -8,26 +8,44 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-
+itemCountSpan.value = 0
+uncheckedCountSpan.value = 0
 function newTodo() {
   /*******************************************************/
-  
+  itemCountSpan.value++
+  uncheckedCountSpan.value++
   var node = document.createElement("li")
   var textnode = document.createTextNode("new item")
   var button = document.createElement("input");
+  var cb = document.createElement("input");
+  cb.type = "checkbox"
+  cb.onclick = function(){
+  	if(cb.checked === true){
+  		uncheckedCountSpan.value--
+  		 document.getElementById("unchecked-count").innerHTML = uncheckedCountSpan.value
+  	}
+  	else{
+  		uncheckedCountSpan.value++
+  		 document.getElementById("unchecked-count").innerHTML = uncheckedCountSpan.value
+  	}
+  }
+  
   button.id = "button";
   button.type = "button";
   button.value = "Remove item";
   button.onclick = function() {
     this.parentElement.remove(this);
+    itemCountSpan.value--
+    document.getElementById("item-count").innerHTML = itemCountSpan.value
+    document.getElementById("unchecked-count").innerHTML = uncheckedCountSpan.value
   }
   node.appendChild(button);
   
   //var but = document.createElement("Button")
   node.appendChild(textnode)
-  //node.appendChild(but)
+  node.appendChild(cb)
   document.getElementById("todo-list").appendChild(node)
-  document.getElementById("item-count").innerHTML = itemCountSpan
+  document.getElementById("item-count").innerHTML = itemCountSpan.value
   
   /********************************************************/
 }
